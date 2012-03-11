9f216238d91bcc30237aab3982bc7d97799cfe6e
Sergio Tulentsev <sergei.tulentsev@gmail.com>
2012-03-11 15:30:15 +0400

    improve capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,49 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+# We're using RVM on a server, need this.
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require 'rvm/capistrano'
+set :rvm_ruby_string, '1.9.3'
+set :rvm_type, :user
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+# Bundler tasks
+require 'bundler/capistrano'
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :application, "sincapun"
+set :repository,  "git@github.com:stulentsev/sincapun.git"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :scm, :git
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+# do not use sudo
+set :use_sudo, false
+set(:run_method) { use_sudo ? :sudo : :run }
+
+# This is needed to correctly handle sudo password prompt
+default_run_options[:pty] = true
+
+set :user, "sergio"
+set :group, user
+set :runner, user
+
+set :host, "#{user}@moskau1" # We need to be able to SSH to that box as this user.
+role :web, host
+role :app, host
+
+set :rails_env, :production
+
+# Where will it be located on a server?
+set :deploy_to, "/srv/#{application}_production"
+set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
+set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
+
+# Unicorn control tasks
+namespace :deploy do
+  task :restart do
+    run "if [ -f #{unicorn_pid} ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
+  end
+  task :start do
+    run "cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
+  end
+  task :stop do
+    run "if [ -f #{unicorn_pid} ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
+  end
+
+end
